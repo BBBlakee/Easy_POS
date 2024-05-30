ba5173c5f380d699545a18b0ad0bc8e8c167b7d2
Michael <77685145+BBBlakee@users.noreply.github.com>
2024-05-30 17:09:48 +0200

    upgrade to Kotlin 2.0.0 and migrated to version catalog (#3)
    * migrated to version catalogs
    
    * upgrade to Kotlin 2.0.0
    
    * update formatter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,9 +3,9 @@ val versionMinor = 0
 val versionPatch = 2
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -46,18 +46,15 @@ android {
 
         compileOptions {
 
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
         kotlinOptions {
-            jvmTarget = "1.8"
+            jvmTarget = "17"
         }
 
         buildFeatures {
             compose = true
-        }
-        composeOptions {
-            kotlinCompilerExtensionVersion = "1.5.3"
         }
         packaging {
             resources {
@@ -68,18 +65,17 @@ android {
 
     dependencies {
 
-        implementation("io.github.pilgr:paperdb:2.7.2")
+        implementation(libs.paperDB)
 
-        implementation("androidx.core:core-ktx:1.13.1")
-        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-        implementation("androidx.activity:activity-compose:1.9.0")
-        implementation(platform("androidx.compose:compose-bom:2024.05.00"))
-        implementation("androidx.compose.ui:ui:1.6.7")
-        implementation("androidx.compose.ui:ui-graphics:1.6.7")
-        implementation("androidx.compose.ui:ui-tooling-preview:1.6.7")
-        implementation("androidx.compose.material3:material3:1.2.1")
-        implementation("androidx.navigation:navigation-compose:2.7.7")
-        implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+        implementation(libs.androidx.ktx)
+        implementation(libs.androidx.runtime.ktx)
+        implementation(libs.androidx.compose)
+        implementation(platform(libs.androidx.compose.bom))
+        implementation(libs.androidx.compose.ui)
+        implementation(libs.androidx.compose.ui.grapics)
+        implementation(libs.androidx.compose.ui.tooling.preview)
+        implementation(libs.androidx.compose.material3)
+        implementation(libs.androidx.navigation)
         //testImplementation("junit:junit:4.13.2")
         //androidTestImplementation("androidx.test.ext:junit:1.1.5")
         //androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
